96af813e86d45eaee7b4d93f7df5fa503eb1a327
wuubangdev <wuubangdev@gmail.com>
2025-12-31 23:00:52 +0700

    up: nav

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -10,7 +10,6 @@ const Hero = async () => {
     const res = await fetchHero();
     const heroData: Hero | null = res.data;
     const listImages = heroData?.listMedia || [];
-    const listSingleImages = heroData?.listSingleMedia || [];
     const mediaBg = heroData?.mediaBg;
     return (
         <section className='w-full py-8 relative overflow-hidden'>
@@ -31,24 +30,6 @@ const Hero = async () => {
                         <CustomCarousel listImage={listImages} />
                     </div>
                     <div className='flex flex-col gap-3'>
-                        {listSingleImages.length > 0 && listSingleImages.map((item, i) => {
-                            if (i > 1) return null;
-                            const seconds = 2 + i * 2;
-                            return (
-                                <div
-                                    key={item.id}
-                                    className='aspect-[21/9] overflow-hidden relative rounded-lg animate-[bounceSoft_infinite]'
-                                    style={{
-                                        animationDuration: `${seconds}s`,
-                                        boxShadow: "rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px"
-                                    }}
-                                >
-                                    <Image alt='carousel' src={fileUrl(item) || "/test.png"} fill style={{ objectFit: 'cover' }}
-                                        className='cursor-pointer hover:scale-105 duration-300'
-                                    />
-                                </div>
-                            )
-                        })}
                     </div>
                 </div>
             </div>
